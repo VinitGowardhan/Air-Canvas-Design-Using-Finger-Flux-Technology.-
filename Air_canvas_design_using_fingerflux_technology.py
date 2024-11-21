81f57bf6a34f02590aac2ab5a0161e501b14cc72
VinitGowardhan <gowardhanvinit@gmail.com>
2024-11-21 19:43:35 +0530

    Update Air_canvas_design_using_fingerflux_technology.py

--- Air_canvas_design_using_fingerflux_technology.py
+++ Air_canvas_design_using_fingerflux_technology.py
@@ -1,29 +1,25 @@
-# All the imports go here
+
 import cv2
 import numpy as np
 import mediapipe as mp
 from collections import deque
 
-# Giving different arrays to handle color points of different color
 bpoints = [deque(maxlen=1024)]
 gpoints = [deque(maxlen=1024)]
 rpoints = [deque(maxlen=1024)]
 ypoints = [deque(maxlen=1024)]
 
-# These indexes will be used to mark the points in particular arrays of specific color
 blue_index = 0
 green_index = 0
 red_index = 0
 yellow_index = 0
 
-# The kernel to be used for dilation purpose
 kernel = np.ones((5, 5), np.uint8)
 
 colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
 colorIndex = 0
 
-# Here is code for Canvas setup
-paintWindow = np.zeros((720, 1280, 3)) + 255  # Increased canvas size
+paintWindow = np.zeros((720, 1280, 3)) + 255  
 paintWindow = cv2.rectangle(paintWindow, (40, 1), (140, 65), (0, 0, 0), 2)
 paintWindow = cv2.rectangle(paintWindow, (160, 1), (255, 65), (255, 0, 0), 2)
 paintWindow = cv2.rectangle(paintWindow, (275, 1), (370, 65), (0, 255, 0), 2)
@@ -37,26 +33,21 @@ cv2.putText(paintWindow, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0,
 cv2.putText(paintWindow, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
 cv2.namedWindow('Paint', cv2.WINDOW_AUTOSIZE)
 
-# Initialize mediapipe
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mpDraw = mp.solutions.drawing_utils
 
-# Initialize the webcam
 cap = cv2.VideoCapture(0)
 ret = True
 
-# Smoothing parameters
 alpha = 0.5
 filtered_center = (0, 0)
 
 while ret:
-    # Read each frame from the webcam
     ret, frame = cap.read()
 
     x, y, c = frame.shape
 
-    # Flip the frame vertically
     frame = cv2.flip(frame, 1)
     framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -71,10 +62,8 @@ while ret:
     cv2.putText(frame, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
     cv2.putText(frame, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
 
-    # Get hand landmark prediction
     result = hands.process(framergb)
 
-    # post-process the result
     if result.multi_hand_landmarks:
         landmarks = []
         for handslms in result.multi_hand_landmarks:
@@ -83,13 +72,11 @@ while ret:
                 lmy = int(lm.y * 480)
                 landmarks.append([lmx, lmy])
 
-            # Drawing landmarks on frames
             mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
         fore_finger = (landmarks[8][0], landmarks[8][1])
         center = fore_finger
         thumb = (landmarks[4][0], landmarks[4][1])
 
-        # Smoothing the hand movements
         filtered_center = (
             int(alpha * filtered_center[0] + (1 - alpha) * center[0]),
             int(alpha * filtered_center[1] + (1 - alpha) * center[1])
@@ -108,7 +95,7 @@ while ret:
             yellow_index += 1
 
         elif center[1] <= 65:
-            if 40 <= center[0] <= 140:  # Clear Button
+            if 40 <= center[0] <= 140:  
                 bpoints = [deque(maxlen=512)]
                 gpoints = [deque(maxlen=512)]
                 rpoints = [deque(maxlen=512)]
@@ -121,16 +108,16 @@ while ret:
 
                 paintWindow[67:, :, :] = 255
             elif 160 <= center[0] <= 255:
-                colorIndex = 0  # Blue
+                colorIndex = 0  
             elif 275 <= center[0] <= 370:
-                colorIndex = 1  # Green
+                colorIndex = 1  
             elif 390 <= center[0] <= 485:
-                colorIndex = 2  # Red
+                colorIndex = 2  
             elif 505 <= center[0] <= 600:
-                colorIndex = 3  # Yellow
+                colorIndex = 3  
         else:
             if colorIndex == 0:
-                bpoints[blue_index].appendleft(filtered_center)  # Use filtered center
+                bpoints[blue_index].appendleft(filtered_center) 
             elif colorIndex == 1:
                 gpoints[green_index].appendleft(filtered_center)
             elif colorIndex == 2:
@@ -148,14 +135,12 @@ while ret:
         ypoints.append(deque(maxlen=512))
         yellow_index += 1
 
-    # Draw lines of all the colors on the canvas and frame
     points = [bpoints, gpoints, rpoints, ypoints]
     for i in range(len(points)):
         for j in range(len(points[i])):
             for k in range(1, len(points[i][j])):
                 if points[i][j][k - 1] is None or points[i][j][k] is None:
                     continue
-                # Use Bresenham's Line Algorithm for smoother lines
                 cv2.line(frame, points[i][j][k - 1], points[i][j][k], colors[i], 2)
                 cv2.line(paintWindow, points[i][j][k - 1], points[i][j][k], colors[i], 2)
 
@@ -165,6 +150,5 @@ while ret:
     if cv2.waitKey(1) == ord('q'):
         break
 
-# release the webcam and destroy all active windows
 cap.release()
 cv2.destroyAllWindows()
